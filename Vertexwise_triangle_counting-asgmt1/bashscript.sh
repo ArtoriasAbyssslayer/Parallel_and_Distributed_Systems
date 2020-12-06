46ab5de8f214f8d9db2b28e9cfe75075ad1bfb9b
Gwyn #harryfil <harry.filis@yahoo.gr>
2020-12-06 04:07:41 +0200

    Update bashscript.sh

--- Vertexwise_triangle_counting-asgmt1/bashscript.sh
+++ Vertexwise_triangle_counting-asgmt1/bashscript.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
 make all
-mkdir log
+mkdir times
 timeNow=$(date +%d-%b-%H_%M_%S)  
 echo "$timeNow"
-touch "log/${timeNow}output.txt"
+touch "times/${timeNow}output.txt"
 
 ## declare an array variable that has the path of every mtx
-declare -a mtx=("./matrixes/belgium_osm/belgium_osm.mtx" "./matrixes/com-Youtube/com-Youtube.mtx" "./matrixes/dblp-2010/dblp-2010.mtx" "./matrixes/mycielskian13/mycielskian13.mtx" "./matrixes/NACA0015/NACA0015.mtx")
+declare -a mtx=("./matrices/belgium_osm/belgium_osm.mtx" "./matrices/com-Youtube/com-Youtube.mtx" "./matrices/dblp-2010/dblp-2010.mtx" "./matrices/mycielskian13/mycielskian13.mtx" "./matrixes/NACA0015/NACA0015.mtx")
 declare -a programs=("./triangle_v3" "./triangle_v3_cilk" "./triangle_v3_opmp")
 
 
@@ -16,13 +16,13 @@ for program in "${programs[@]}"
 do
     for matrix in "${mtx[@]}"
         do
-            echo "$program" "$matrix" 1 2 >> "log/${timeNow}output.txt"
-            "$program" "$matrix" 1 2 >> "log/${timeNow}output.txt"
+            echo "$program" "$matrix" 1 2 >> "times/${timeNow}output.txt"
+            "$program" "$matrix" 1 2 >> "times/${timeNow}output.txt"
             # Add a blank line between each iteration 
-            echo >> "log/${timeNow}output.txt"
+            echo >> "times/${timeNow}output.txt"
         done
         # Add a blank line between each iteration 
-        echo >> "log/${timeNow}output.txt"    
+        echo >> "times/${timeNow}output.txt"    
 done
 
 
