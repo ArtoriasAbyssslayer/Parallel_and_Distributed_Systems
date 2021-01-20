44e19a057b2a1b8dbf6f6bc6a354e3ac3212c0bd
Gwyn <harry.filis@yahoo.gr>
2021-01-20 18:31:57 +0200

    Create v0.m

---
+++ KnnSearch/matlab/v0.m
@@ -0,0 +1,33 @@
+% todo Generate random 2D X and Y arrays
+n = 100 
+m = 10
+d = 2
+k = 5
+
+min = 0
+max = 100
+randomBounded = (max-min).*rand(1000,1) + min;
+
+X = zeros(n,d); (calloc X)
+for i = 1:d
+  X(:,i) = (max - min).*rand(n,1) + min;
+end
+
+Y = zeros(m,d);
+for i = 1:d
+  Y(:,i) = (max-min).*rand(m,1) + a;
+end
+
+% Calculate D
+a = sum(X.^2,2);
+b = - 2 * X*Y.';
+c = sum(Y.^2,2).';
+D = sqrt(sum(X.^2,2) - 2 * X*Y.' + sum(Y.^2,2).');
+min_matrix = zeros(k, m);
+for col = 1:m
+    for i = 1:k
+        [min_matrix(i,col), index] = min(D(:,col));
+        D(index,col) = inf;
+    end
+end
+
